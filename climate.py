65d951986a7813e182b078a9fced606aeecb1df1
echoromeo <post@undereksponert.no>
2020-05-17 01:21:46 +0200

    pip

--- climate.py
+++ climate.py
@@ -27,7 +27,7 @@ from homeassistant.components.climate.const import (
     PRESET_COMFORT
 )
 from homeassistant.components.climate import ClimateDevice
-from .pynobo.nobo import nobo
+from pynobo import nobo
 
 #REQUIREMENTS = ['time', 'warnings', 'logging', 'socket', 'threading']
 
@@ -225,7 +225,5 @@ class AwesomeHeater(ClimateDevice):
                         self._current_mode = HVAC_MODE_HEAT
 
         self._current_temperature = self._nobo.get_current_zone_temperature(self._id)
-        if self._current_temperature == 'N/A':
-            self._current_temperature = None
         self._target_temperature_high = int(self._nobo.zones[self._id]['temp_comfort_c'])
         self._target_temperature_low = int(self._nobo.zones[self._id]['temp_eco_c'])        
